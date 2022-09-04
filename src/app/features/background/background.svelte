50dd4d7e228e0e2a81bef5817a76f43a46e107fe
Amaral Krichman <amaralkrichman@gmail.com>
2022-09-04 00:54:53 -0300

    background.svelte: finish basic structure

--- src/app/features/background/background.svelte
+++ src/app/features/background/background.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
+  import type { BackgroundPhoto } from 'src/types';
+  import { bgData as photo } from 'src/app/stores';
 
-  // call the middleware here to get the today's img from bg script
+
+  console.log('background.svelte');
+
+
+  let loaded = false;
 
 </script>
 
-<div class="background">
-  <img src="" alt="">
+<div class="background" class:loaded >
+    <!-- <img class='previous'
+      on:load|once={() => loaded = true}
+      src={photo?.src}
+      alt={photo?.alt_description}
+    > -->
+    <img class='current'
+      on:load|once={() => loaded = true}
+      src={$photo?.src}
+      alt={$photo?.alt_description}
+    >
 </div>
 
 <style>
   .background {
     position: fixed;
     inset: 0;
-    background: blue;
+    background: white;
     z-index: 0;
   }
+  img {
+    opacity: 0;
+    transition: opacity .8s;
+  }
+  .loaded img { opacity: 1; }
 </style>
