72849b32116ee68cd1b47123af759d32839ddf0a
Amaral Krichman <amaralkrichman@gmail.com>
2022-09-04 00:56:19 -0300

    App.svelte: Basic layout - Request new bg button - global styles

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+  import './app.css';
+  import { requestNewBackground } from '@lib/middleware';
   import Background from '@features/background';
+
+
+function printDate(){
+  const now = new Date();
+  const hours = now.getHours();
+  const mins = now.getMinutes();
+  const hh = String(hours).padStart(2, '0');
+  const mm = String(mins).padStart(2, '0');
+
+  return `${hh}:${mm}`;
+}
 
 </script>
 
@@ -7,8 +20,10 @@
   <Background />
   <div class='layout'>
     <div class='layout-content'>
-      <h1>Lobby</h1>
+      <div class='time'><span>{printDate()}</span></div>
+      <div class='greeting'><span>Hello, Amaral</span></div>
 
+      <button on:click={requestNewBackground}>New background</button>
     </div>
 
   </div>
@@ -22,5 +37,35 @@
   }
   .layout-content {
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    color: white;
+    background:
+      radial-gradient(rgb(0 0 0 / 70%) -20%, transparent 30%),
+      radial-gradient(transparent 80%, rgb(0 0 0 / 51%) 125%);
+    gap: 1em;
+  }
+  .time {
+    font-size: 11rem;
+  }
+  .greeting {
+    font-size: 4rem;
+  }
+  .time,.greeting {
+    font-weight: 500;
+    line-height: 1;
+  }
+  button {
+    all: unset;
+    cursor: pointer;
+    padding: 1rem;
+    background-color: rgb(0 0 0 / 25%);
+    border-radius: 2em;
+  }
+  button:hover {
+    background-color: rgb(0 0 0 / 40%);
   }
 </style>
