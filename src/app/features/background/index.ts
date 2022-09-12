55426fc34bc7e907795b62c48430efa114ac5db0
Amaral Krichman <amaralkrichman@gmail.com>
2022-09-11 23:32:38 -0300

    fix: Missed index export for NewBackground

--- src/app/features/background/index.ts
+++ src/app/features/background/index.ts
@@ -1,7 +1,9 @@
 import Background from './background.svelte';
+import RequestButton from './request-new-btn.svelte';
 import AuthorWidget from './author-widget.svelte';
 
 export default Background;
 export {
+  RequestButton,
   AuthorWidget,
 };
