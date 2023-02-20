6a73c2638412cf7530519a4a33c8f3e766bc4ee9
Amaral Krichman <amaralkrichman@gmail.com>
2023-02-19 22:15:41 -0300

    feat: add InputSelect and InputText

---
+++ src/app/features/ui/input-select.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import { tick } from 'svelte';
+  import type { SettingsOption } from 'src/types';
+  import InputText from './input-text.svelte';
+  import {
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+  } from "@rgossiaux/svelte-headlessui";
+
+  export let options: SettingsOption[] = [
+    {
+      label: 'Default',
+      value: 'default',
+    },
+  ];
+
+  let currentValue = options[0].value;
+  $: selectedOption = options.find((o) => o.value === currentValue);
+
+  export let onChange: (opt: SettingsOption) => void;
+
+  async function handleChange(e: CustomEvent) {
+    currentValue = e.detail;
+    await tick();
+    onChange(selectedOption);
+  }
+
+</script>
+
+<div class="ui-input-select">
+  <Listbox
+    value={currentValue}
+    on:change={handleChange}
+  >
+    <ListboxButton class="option-button">
+      {selectedOption.label}
+    </ListboxButton>
+    <ListboxOptions class="option-list">
+      {#each options as option (option.value)}
+        {@const value = `${option.value}`}
+        <ListboxOption
+          {value}
+          class={({ active, selected }) => {
+            const act = active ? 'active' : '';
+            const sel = selected ? 'selected' : '';
+            return ['option', act, sel].join(' ');
+          }}
+        >
+          {option.label}
+        </ListboxOption>
+      {/each}
+    </ListboxOptions>
+  </Listbox>
+</div>
+
+<style>
+  .ui-input-select {
+    display: flex;
+    font-size: 14px;
+    position: relative;
+    width: 10rem;
+  }
+  :global(.ui-input-select > div) {
+    width: 100%;
+  }
+  :global(.ui-input-select .option-button) {
+    all: unset;
+    padding: .5rem 1rem;
+    border-radius: var(--border-radius-s);
+    border: 1px solid rgb(255 255 255 / .08);
+    cursor: pointer;
+    text-align: left;
+    width: -webkit-fill-available;
+    width: fill-available;
+  }
+  :global(.ui-input-select .option-list) {
+    list-style: none;
+    margin-block: 0;
+    padding-inline: 0;
+    overflow: hidden;
+    width: 100%;
+    background-color: rgb(0 0 0 / .2);
+    border-radius: var(--border-radius-s);
+    border: 1px solid rgb(255 255 255 / .08);
+    position: absolute;
+    transform: translateY(.4rem);
+    box-shadow: 0 1.2em 1.3em -0.8em rgb(0 0 0 / .3);
+  }
+  :global(.ui-input-select .option) {
+    cursor: pointer;
+    padding: .5rem 1rem;
+    background-color: rgb(0 0 0 / 15%);
+    transition: background-color .2s;
+  }
+  :global(.ui-input-select .option.active) {
+    background-color: rgb(255 255 255 / 3%);
+  }
+  :global(.ui-input-select .option.selected) {
+    font-weight: 500;
+    background-color: rgb(255 255 255 / 33%);
+  }
+
+</style>
---
+++ src/app/features/ui/input-text.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+
+  export let key = '';
+  export let label = '';
+  export let value = '';
+
+</script>
+
+<div class="ui-input-text">
+  <label for="{key}">
+    {label}
+    <input type="text" name="{key}" id="" {value} on:change>
+  </label>
+</div>
+
+<style>
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+    font-size: 12px;
+    font-weight: normal;
+  }
+  input {
+    padding: .5rem;
+    width: 80%;
+    border: none;
+    border-bottom: 1px solid rgb(255 255 255 / 30%);
+    border-radius: .2rem;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    background: none;
+    font-size: 1rem;
+    font-family: inherit;
+    color: white;
+    transition: all .2s;
+  }
+  input:focus-visible {
+    outline: none;
+    border-bottom-color: rgb(255 255 255 / 70%);
+    background-color: rgb(0 0 0 / 15%);
+  }
+</style>
